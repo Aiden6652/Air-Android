e4c79084d85d8411cc7e12942905990a31279d70
tomikun <60690056+alexytomi@users.noreply.github.com>
2026-07-27 01:22:42 +0800

    fix: Disable SDL3 JNI_OnLoad from running outside dalvikvm
    Should fix controlify swapping from jna to jni

--- app_pojavlauncher/src/main/jni/native_hooks/dlopen_hook.c
+++ app_pojavlauncher/src/main/jni/native_hooks/dlopen_hook.c
@@ -10,12 +10,22 @@
 #include <string.h>
 
 typedef void *(*dlopen_func_t)(const char *, int);
-// Maybe make this a blacklist instead of a whitelist??
-static const char *const redirected_libs[] = {
-        "libSDL3.so",
-        "libSDL2.so",
+typedef void *(*dlsym_func_t)(void *, const char *);
+typedef jint (*JNI_OnLoad_t)(JavaVM *vm, void *reserved);
+
+#define SDL_LIBS \
+    "libSDL3.so", \
+    "libSDL2.so"
+
+static const char *const sdl_libs[] = {
+        SDL_LIBS
 };
 
+static const char *const redirected_libs[] = {
+        SDL_LIBS,
+};
+
+// Strip the full paths of specific natives so we look inside LD_LIBRARY_PATH instead
 static const char *redirect_dlopen_path(const char *filename) {
     if (filename == NULL)
         return NULL;
@@ -33,20 +43,65 @@ static const char *redirect_dlopen_path(const char *filename) {
     return filename;
 }
 
+static void *sdl3_handle = NULL;
+static JNI_OnLoad_t orig_sdl3_JNI_OnLoad;
+
+static bool ifSdl(const char *filename) {
+    if (filename == NULL)
+        return false;
+
+    const char *basename = strrchr(filename, '/');
+    basename = basename ? basename + 1 : filename;
+
+    for (size_t i = 0; i < sizeof(sdl_libs) / sizeof(sdl_libs[0]); ++i) {
+        if (strcmp(basename, sdl_libs[i]) == 0){
+            return true;
+        }
+    }
+    return false;
+}
+
+// Skip if not in dalvik vm cause register_methods needs to be ran in android land
+static jint custom_sdl3_JNI_OnLoad(JavaVM *vm, void *reserved){
+    if (pojav_environ->dalvikJavaVMPtr == vm) {
+        return orig_sdl3_JNI_OnLoad(vm, reserved);
+    }
+    return JNI_VERSION_1_4;
+}
+
 void *custom_dlopen(const char *filename, int flags) {
     void *result = BYTEHOOK_CALL_PREV(
             custom_dlopen,
             dlopen_func_t,
             redirect_dlopen_path(filename),
             flags);
+    if (ifSdl(filename)) sdl3_handle = result;
 
     BYTEHOOK_POP_STACK();
     return result;
 }
 
-void create_dlopen_hooks(bytehook_hook_all_t bytehook_hook_all_p) {
-    bytehook_stub_t stub =
-            bytehook_hook_all_p(NULL, "dlopen", &custom_dlopen, NULL, NULL);
+void *custom_dlsym(void *handle, const char *symbol) {
+    void *result = BYTEHOOK_CALL_PREV(
+            custom_dlsym,
+            dlsym_func_t,
+            handle,
+            symbol);
+    BYTEHOOK_POP_STACK();
 
-    LOGI("Successfully initialized dlopen hook, stub: %p", stub);
+    if (sdl3_handle && handle == sdl3_handle && strcmp(symbol, "JNI_OnLoad") == 0) {
+        orig_sdl3_JNI_OnLoad = (JNI_OnLoad_t) result;
+        // This outputs in the minecraft logs
+        LOGI("Amethyst-Android: Intercepted SDL3 JNI_OnLoad: %p", result);
+        return (void *) custom_sdl3_JNI_OnLoad;
+    }
+    return result;
+}
+
+void create_dlopen_hooks(bytehook_hook_all_t bytehook_hook_all_p) {
+    bytehook_stub_t stub_dlopen =
+            bytehook_hook_all_p(NULL, "dlopen", &custom_dlopen, NULL, NULL);
+    bytehook_stub_t stub_dlsym =
+            bytehook_hook_all_p(NULL, "dlsym", &custom_dlsym, NULL, NULL);
+    LOGI("Successfully initialized dlopen hooks, stub: %p %p", stub_dlopen, stub_dlsym);
 }
